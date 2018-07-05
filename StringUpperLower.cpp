20a89b7bd1981a973f7e03e3ef79f8651e1b90ae
ying xin <395488553@qq.com>
2018-07-05 18:39:03 +0800

    Update StringUpperLower.cpp

--- StringUpperLower.cpp
+++ StringUpperLower.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <iterator>
 using namespace std;
 
 #include <ctype.h>
@@ -8,16 +7,12 @@ using namespace std;
 //1.raw string literals R"(..)"
 std::string no_newlines=R"(\n\n)";
 std::string cmd(R"(cd "d:\")");
+
 //2.string to upper, or to lower
 std::string str = "djwoJ*FALInxk.lfDF232Dg";
 
 int stringPro()
 {
-    int a2, b2, a3;
-    int *c2 = &a2, *c3 = &a3, *d2 = &b2;
-    std::cout << std::hex << c2 << " " << d2 << std::endl;
-    std::cout << std::hex << std::distance(d2, c3) << std::endl;//std::distance返回的是迭代器的距离，而不是地址之差
-
     cout<<no_newlines<<cmd<<endl; //\n\ncd "d:\"
 
     //old methods
